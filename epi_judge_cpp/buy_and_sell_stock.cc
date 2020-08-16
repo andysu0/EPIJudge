e245470423ac5ff5fae834105cab40d2aac619b7
Andy <andy.z.su@outlook.com>
2020-08-16 02:40:43 -0400

    5.6 Buy & Sell Stock Once

--- epi_judge_cpp/buy_and_sell_stock.cc
+++ epi_judge_cpp/buy_and_sell_stock.cc
@@ -3,8 +3,22 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 double BuyAndSellStockOnce(const vector<double>& prices) {
-  // TODO - you fill in here.
-  return 0.0;
+
+  double bestProfit = 0.0;
+  size_t buyIdx = 0;
+
+  for (size_t i = 0; i < prices.size(); ++i)
+  {
+    const double currentprofit = prices[i] - prices[buyIdx];
+    bestProfit = std::max(currentprofit, bestProfit);
+
+    if (prices[buyIdx] > prices[i])
+    {
+      buyIdx = i;
+    }
+  }
+
+  return bestProfit;
 }
 
 int main(int argc, char* argv[]) {
