71ff1d5117ccdc30f194e4b9b90ec83c64323e47
Andy <andy.z.su@outlook.com>
2020-08-18 17:24:29 -0400

    10.1 Merge sorted arrays

--- epi_judge_cpp/sorted_arrays_merge.cc
+++ epi_judge_cpp/sorted_arrays_merge.cc
@@ -2,9 +2,45 @@
 
 #include "test_framework/generic_test.h"
 using std::vector;
+
+struct HeadNode
+{
+  int value;
+  size_t arrNum;
+  size_t arrIdx;
+};
+
 vector<int> MergeSortedArrays(const vector<vector<int>>& sorted_arrays) {
-  // TODO - you fill in here.
-  return {};
+  auto cmp = [](const HeadNode& lhs, const HeadNode& rhs)
+  {
+    return lhs.value > rhs.value;
+  };
+  std::priority_queue<HeadNode, vector<HeadNode>, decltype(cmp)> minHeap(cmp);
+  vector<int> result;
+
+  for (size_t i = 0; i < sorted_arrays.size(); ++i)
+  {
+    if (!sorted_arrays[i].empty())
+    {
+      minHeap.push({sorted_arrays[i][0], i, 0});
+    }
+  }
+
+  while (!minHeap.empty())
+  {
+    const auto& node = minHeap.top();
+    result.push_back(node.value);
+
+    size_t nextIdxInArr = node.arrIdx + 1;
+    if (nextIdxInArr < sorted_arrays[node.arrNum].size())
+    {
+      minHeap.push({sorted_arrays[node.arrNum][nextIdxInArr], node.arrNum, nextIdxInArr});
+    }
+
+    minHeap.pop();
+  }
+
+  return result;
 }
 
 int main(int argc, char* argv[]) {
