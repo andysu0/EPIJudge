4007c5555d998c98b889d91912881d20c4440d15
Andy <andy.z.su@outlook.com>
2020-08-18 23:32:24 -0400

    12.0 Anagrams

--- epi_judge_cpp/anagrams.cc
+++ epi_judge_cpp/anagrams.cc
@@ -1,13 +1,31 @@
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 #include "test_framework/generic_test.h"
 using std::string;
 using std::vector;
 
 vector<vector<string>> FindAnagrams(const vector<string>& dictionary) {
-  // TODO - you fill in here.
-  return {};
+  std::unordered_map<string, vector<string>> map;
+
+  for (const auto& s : dictionary)
+  {
+    string sorted_s(s);
+    std::sort(sorted_s.begin(), sorted_s.end());
+    map[sorted_s].push_back(s);
+  }
+
+  vector<vector<string>> result;
+  for (const auto& item : map)
+  {
+    if (item.second.size() > 1)
+    {
+      result.push_back(item.second);
+    }
+  }
+
+  return result;
 }
 
 int main(int argc, char* argv[]) {
