7076096938769dc19d8375564f8e303c6ca630ac
Andy <andy.z.su@outlook.com>
2020-08-22 19:03:12 -0400

    Score Combinations

--- epi_judge_cpp/number_of_score_combinations.cc
+++ epi_judge_cpp/number_of_score_combinations.cc
@@ -4,9 +4,33 @@
 using std::vector;
 
 int NumCombinationsForFinalScore(int final_score,
-                                 const vector<int>& individual_play_scores) {
-  // TODO - you fill in here.
-  return 0;
+                                 const vector<int>& individual_play_scores)
+{
+  vector<vector<int>> cache;
+  cache.resize(individual_play_scores.size(), std::vector<int>(final_score + 1, 0));
+
+  for (int i = 0; i < individual_play_scores.size(); ++i)
+  {
+    cache[i][0] = 1;
+    for (int j = 1; j <= final_score; ++j)
+    {
+      int combi = 0;
+
+      if (i - 1 >= 0)
+      {
+        combi += cache[i - 1][j];
+      }
+
+      if (j - individual_play_scores[i] >= 0)
+      {
+        combi += cache[i][j - individual_play_scores[i]];
+      }
+
+      cache[i][j] = combi;
+    }
+  }
+
+  return cache.back().back();
 }
 
 int main(int argc, char* argv[]) {
