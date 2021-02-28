29b50bec200e9a59e12205f99dc9a328ccf9c376
Andy <andy.z.su@outlook.com>
2021-02-27 21:48:19 -0800

    5.5

--- epi_judge_cpp/sorted_array_remove_dups.cc
+++ epi_judge_cpp/sorted_array_remove_dups.cc
@@ -3,11 +3,31 @@
 #include "test_framework/generic_test.h"
 #include "test_framework/timed_executor.h"
 using std::vector;
+
+// [1 1 1 2 2 3 3]
+// [1 2 3]
+
 // Returns the number of valid entries after deletion.
 int DeleteDuplicates(vector<int>* A_ptr) {
-  // TODO - you fill in here.
-  return 0;
+    auto& A = *A_ptr;
+    if (A.empty())
+        return 0;
+
+    int writeIdx = 1;
+    for (size_t i = 0; i < A.size(); ++i)
+    {
+        if (A[writeIdx - 1] != A[i])
+        {
+            A[writeIdx] = A[i];
+            ++writeIdx;
+        }
+    }
+
+    A.resize(writeIdx);
+
+    return writeIdx;
 }
+
 vector<int> DeleteDuplicatesWrapper(TimedExecutor& executor, vector<int> A) {
   int end = executor.Run([&] { return DeleteDuplicates(&A); });
   A.resize(end);
